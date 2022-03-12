b264350f268a6febf082407db5a0941b1ee294c1
razqyaqil <razqyaqil2@gmail.com>
2022-03-12 16:04:11 +0700

    menambahkan class KodeBuah

--- modul3_1302202072/Program.cs
+++ modul3_1302202072/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace modul3_1302202072 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("PISANG :",kodeBuah.getKodeBuah(kodeBuah.buah.Pisang));
+
+        }
+    }
+}
---
+++ modul3_1302202072/kodeBuah.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302202072
+{
+    public class kodeBuah
+    {
+        public enum buah
+        {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka
+        }
+
+        public static string getKodeBuah(buah namaBuah)
+        {
+            string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00" };
+            
+            return kodeBuah[(int) namaBuah];
+
+        }
+    }
+}
